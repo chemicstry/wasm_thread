9d50823f9dd5327081b1fa2ed8a274590cdb3592
chemicstry <chemicstry@gmail.com>
2023-03-10 14:55:46 +0200

    Remove unneeded flags in scripts

--- examples-wasm-pack/web-build-no-module.ps1
+++ examples-wasm-pack/web-build-no-module.ps1
@@ -1,2 +1,1 @@
-$env:RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals"
 wasm-pack build --dev --out-dir ./no-module/target --target no-modules
--- examples-wasm-pack/web-build.sh
+++ examples-wasm-pack/web-build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals" \
-    wasm-pack build --dev --out-dir ./module/target --target web --features wasm_thread/es_modules
+wasm-pack build --dev --out-dir ./module/target --target web --features wasm_thread/es_modules
