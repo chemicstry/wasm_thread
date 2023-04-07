dad888c59c248c300e91d7184d976b997bc6991f
Erwan VIVIEN <vivien.erwan@gmail.com>
2023-04-07 13:58:43 +0200

    feat: added tests

--- tests/native.rs
+++ tests/native.rs
@@ -1,5 +1,10 @@
 //! Trivial tests to ensure that native thread API is unchanged.
 
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    time::Duration,
+};
+
 use wasm_thread as thread;
 
 #[test]
@@ -37,4 +42,73 @@ fn thread_scope() {
     // After the scope, we can modify and access our variables again:
     a.push(4);
     assert_eq!(x, a.len());
+}
+
+#[test]
+fn thread_messaging() {
+    use std::sync::mpsc::{channel, Receiver};
+
+    let (tx, rx) = channel();
+    static ATOMIC_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    fn reader_callback(rx: Receiver<String>) {
+        while let Ok(_) = rx.recv() {
+            let old_value = ATOMIC_COUNT.fetch_add(1, Ordering::Relaxed);
+            if old_value == usize::MAX {
+                break;
+            }
+            thread::sleep(Duration::from_millis(200));
+        }
+    }
+
+    let reader_thread = thread::Builder::new()
+        .name(String::from("reader"))
+        .spawn(|| reader_callback(rx))
+        .unwrap();
+
+    for i in 0..1000 {
+        tx.send(format!("message {}", i)).unwrap();
+    }
+
+    let _ = thread::spawn(move || {
+        drop(tx);
+        thread::sleep(Duration::from_millis(1100));
+
+        let value = ATOMIC_COUNT.load(Ordering::Relaxed);
+        std::assert_eq!(value, 6);
+        ATOMIC_COUNT.store(usize::MAX, Ordering::Relaxed);
+    })
+    .join()
+    .unwrap();
+
+    reader_thread.join().unwrap();
+}
+
+#[test]
+fn thread_no_join() {
+    use std::sync::{atomic::AtomicBool, mpsc::channel};
+
+    let (tx, rx) = channel();
+    static ATOMIC_STARTED: AtomicBool = AtomicBool::new(false);
+
+    let _ = thread::Builder::new()
+        .name(String::from("polled"))
+        .spawn(move || {
+            ATOMIC_STARTED.store(true, Ordering::Relaxed);
+            rx.recv().unwrap();
+        })
+        .unwrap();
+
+    let _ = thread::Builder::new()
+        .name(String::from("awaiter"))
+        .spawn(move || {
+            thread::sleep(Duration::from_millis(1000));
+            let started = ATOMIC_STARTED.load(Ordering::Relaxed);
+            std::assert_eq!(started, true);
+        })
+        .unwrap()
+        .join()
+        .unwrap();
+
+    tx.send(()).unwrap();
 }
