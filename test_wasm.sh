ff3628ab924e3b5b2fe82fe87ee6bb12a8b8627a
chemicstry <chemicstry@gmail.com>
2024-03-17 14:04:17 +0200

    chrome driver seems broken at the moment

--- test_wasm.sh
+++ test_wasm.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
-	wasm-pack test --headless --firefox --chrome -- -Z build-std=panic_abort,std
+	wasm-pack test --headless --firefox -- -Z build-std=panic_abort,std
